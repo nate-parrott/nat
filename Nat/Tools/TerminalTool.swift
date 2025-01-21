b37cece85ceaae7ddede80e07f71240c4d2dca5b
nate <nateparro2t@gmail.com>
2025-01-20 19:13:43 -0500

    wip

--- Nat/Tools/TerminalTool.swift
+++ Nat/Tools/TerminalTool.swift
@@ -36,7 +36,8 @@ struct TerminalTool: Tool {
         Execute a terminal command and return its output. 
         Only use this for commands that will complete quickly (within 10 minutes) like running tests, interacting with git, etc. 
         NEVER use for long-running or indefinite programs like servers, processes, interactive programs.
-        Always add -y or --yes flags when available to avoid prompts.
+        Always add -y or --yes flags when available to avoid prompts and interactivity.
+        Use --no-pager for git show and related ops.
         """, type: Args.self)
     
     struct Args: FunctionArgs {
