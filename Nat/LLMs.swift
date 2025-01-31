36ef995bb7e2fe3d3a853b77c7fdaf8f64bb5394
nate <nateparro2t@gmail.com>
2025-01-31 13:04:11 -0500

    diff

--- Nat/LLMs.swift
+++ Nat/LLMs.swift
@@ -4,8 +4,8 @@ import Foundation
 enum LLMs {
     static func smartAgentModel() throws -> ChatGPT {
         try ChatGPT(credentials: .init(apiKey: openrouterKey()), options: .init(temp: 1, model: .custom("anthropic/claude-3.5-sonnet:beta", 200_000), baseURL: .openRouterOpenAIChatEndpoint))
-//        try ChatGPT(credentials: .init(apiKey: openrouterKey()), options: .init(temp: 1, model: .custom("deepseek/deepseek-r1-distill-llama-70b", 130_000), printToConsole: true, baseURL: .openRouterOpenAIChatEndpoint))
-//        try ChatGPT(credentials: .init(apiKey: openrouterKey()), options: .init(temp: 1, model: .custom("openai/o1-mini-2024-09-12", 64_000), baseURL: .openRouterOpenAIChatEndpoint))
+//        try ChatGPT(credentials: .init(apiKey: openrouterKey()), options: .init(temp: 1, model: .custom("deepseek/deepseek-r1:nitro", 160_000), printToConsole: true, baseURL: .openRouterOpenAIChatEndpoint))
+//        try ChatGPT(credentials: .init(apiKey: openrouterKey()), options: .init(temp: 1, model: .custom("google/gemini-2.0-flash-thinking-exp:free", 1_000_000), baseURL: .openRouterOpenAIChatEndpoint))
     }
 
     static var fakeFunctions: Bool {
