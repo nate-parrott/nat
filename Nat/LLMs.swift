9a7a0b6fbb5076efd2070af193d8418494a1058a
Nate Parrott <nate@nateparrott.com>
2025-02-06 10:51:55 -0500

    ok

--- Nat/LLMs.swift
+++ Nat/LLMs.swift
@@ -3,9 +3,7 @@ import Foundation
 
 enum LLMs {
     static func smartAgentModel() throws -> ChatGPT {
-        try ChatGPT(credentials: .init(apiKey: openrouterKey()), options: .init(temp: 1, model: .custom("anthropic/claude-3.5-sonnet:beta", 200_000), baseURL: .openRouterOpenAIChatEndpoint))
-//        try ChatGPT(credentials: .init(apiKey: openrouterKey()), options: .init(temp: 1, model: .custom("deepseek/deepseek-r1:nitro", 160_000), printToConsole: true, baseURL: .openRouterOpenAIChatEndpoint))
-//        try ChatGPT(credentials: .init(apiKey: openrouterKey()), options: .init(temp: 1, model: .custom("google/gemini-2.0-flash-thinking-exp:free", 1_000_000), baseURL: .openRouterOpenAIChatEndpoint))
+        try ChatGPT(credentials: .init(apiKey: openrouterKey()), options: .init(temp: 1, model: .custom("anthropic/claude-3.5-sonnet:beta", 200_000), baseURL: .openRouterOpenAIChatEndpoint, headers: additionalHeaders))
     }
 
     static var fakeFunctions: Bool {
@@ -14,16 +12,17 @@ enum LLMs {
     
     static var additionalHeaders: [String: String] {
         [
-            "HTTP-Referer": "https://github.com/nate-parrott/nat"
+            "HTTP-Referer": "https://github.com/nate-parrott/nat",
+            "X-Title": "Nat for Xcode",
         ]
     }
 
     static func quickModel() throws -> ChatGPT {
-        try ChatGPT(credentials: .init(apiKey: openrouterKey()), options: .init(temp: 1, model: .custom("openai/gpt-4o-mini", 1_000_000), baseURL: .openRouterOpenAIChatEndpoint))
+        try ChatGPT(credentials: .init(apiKey: openrouterKey()), options: .init(temp: 1, model: .custom("openai/gpt-4o-mini", 1_000_000), baseURL: .openRouterOpenAIChatEndpoint, headers: additionalHeaders))
     }
 
     static func applierModel() throws -> ChatGPT {
-        try ChatGPT(credentials: .init(apiKey: openrouterKey()), options: .init(temp: 0, model: .custom("openai/gpt-4o-mini", 1_000_000), baseURL: .openRouterOpenAIChatEndpoint))
+        try ChatGPT(credentials: .init(apiKey: openrouterKey()), options: .init(temp: 0, model: .custom("openai/gpt-4o-mini", 1_000_000), baseURL: .openRouterOpenAIChatEndpoint, headers: additionalHeaders))
     }
 
 
